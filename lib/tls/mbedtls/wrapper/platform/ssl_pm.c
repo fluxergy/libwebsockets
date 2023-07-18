e5dced42dc4cf24d89b33ce30d9157f91413a25b
saltyJeff <saltyJeff@users.noreply.github.com>
2023-07-18 16:07:56 -0700

    update constant for mbedtls v3.4.0

--- lib/tls/mbedtls/wrapper/platform/ssl_pm.c
+++ lib/tls/mbedtls/wrapper/platform/ssl_pm.c
@@ -549,7 +549,7 @@ OSSL_HANDSHAKE_STATE ssl_pm_get_state(const SSL *ssl)
         case MBEDTLS_SSL_SERVER_KEY_EXCHANGE:
             state = TLS_ST_SR_KEY_EXCH;
             break;
-        case MBEDTLS_SSL_SERVER_NEW_SESSION_TICKET:
+        case MBEDTLS_SSL_NEW_SESSION_TICKET:
             state = TLS_ST_SW_SESSION_TICKET;
             break;
         case MBEDTLS_SSL_SERVER_HELLO_VERIFY_REQUEST_SENT:
